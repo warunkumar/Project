28dd92a50a082d702c2fb9d972fb2bcae850744f
LAPTOP-VKB1C41Q\warun <waruntech@gmail.com>
2023-05-17 13:52:41 +0530

    Code updated

---
+++ Demo/Springboot VueJS Bank application/Bank Application Frontend/bank-application-demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <BankDashboard/>
+  <CustomerRegistration/>
+</template>
+
+<script>
+import BankDashboard from './components/BankDashboard.vue'
+import Login from './components/Login.vue'
+import CustomerRegistration from './components/CustomerRegistration.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BankDashboard,
+    Login,
+    CustomerRegistration
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Demo/Springboot VueJS Bank application/Bank Application Frontend/bank-application-demo/src/components/BankDashboard.vue
@@ -0,0 +1,44 @@
+<template>
+
+<div>
+  dddd
+</div>
+
+<div>
+  
+</div>
+
+</template>
+
+<script>
+//import Login from './components/Login.vue'
+//import CustomerRegistration from './components/CustomerRegistration.vue'
+export default {
+  name: 'HelloWorld3',
+  props: {
+    msg: String
+  },
+  components: {
+    //Login
+    //CustomerRegistration,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
